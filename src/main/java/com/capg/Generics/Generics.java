5c2156bcf98e72c46235793d4446295f6f9975b7
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-12 20:07:28 +0530

    Refactored to String with comparator

--- src/main/java/com/capg/Generics/Generics.java
+++ src/main/java/com/capg/Generics/Generics.java
@@ -1,14 +1,60 @@
 package com.capg.Generics;
 
-public class Generics {
+import java.util.Arrays;
+
+public class Generics<T extends Comparable<T>> {
+	
+	T a;
+	T b;
+	T c;
+	T[] args;
+	@SafeVarargs
+	public Generics(T...args) {
+		this.args = args;
+	}
+	
+	public T maxNum() {
+		T test = maxNum(args);
+		return test;
+		
+	}
+	public T maxNum(T...args) {
+		T max;
+		Arrays.sort(args);
+		max = args[args.length - 1];
+		return max;
+	}
+	
+	
+	
+	/*public Integer maxNum (Integer first, Integer second, Integer third) {
+		
+		if(first.compareTo(second) >= 0 && first.compareTo(third) >= 0)
+			return first;
+		else if(second.compareTo(first) >= 0 && second.compareTo(third) >= 0)
+			return second;
+		else return third;
+	}
+	
 	public Float maxNum (Float first, Float second, Float third) {
+		
+		if(first.compareTo(second) >= 0 && first.compareTo(third) >= 0)
+			return first;
+		else if(second.compareTo(first) >= 0 && second.compareTo(third) >= 0)
+			return second;
+		else return third;
+	}
+	
+	public String maxNum (String first, String second, String third) {
 		
 			if(first.compareTo(second) >= 0 && first.compareTo(third) >= 0)
 				return first;
 			else if(second.compareTo(first) >= 0 && second.compareTo(third) >= 0)
 				return second;
 			else return third;
-		}
+		}*/
+	
+	
 
 }
 	
--- src/test/java/com/Generics/GenericsTest.java
+++ src/test/java/com/Generics/GenericsTest.java
@@ -7,32 +7,81 @@ import com.capg.Generics.Generics;
 
 
 public class GenericsTest {
-
+	
 	@Test
 	public void giveMaxNumAtFirstPosition() {
-		Generics test = new Generics();
-		Float maxNum = test.maxNum(10.2F, 5.0F, 3.5F);
-		Float expec = 10.2F;
+		Generics test = new Generics(10, 5, 3);
+		Integer maxNum = (Integer) test.maxNum();
+		Integer expec = 10;
 		assertEquals(expec, maxNum);
 			
 	}
 	
 	@Test
 	public void giveMaxNumAtSecondPosition() {
-		Generics test = new Generics();
-		Float maxNum = test.maxNum(6.5F, 12.4F, 3.6F);
-		Float expec = 12.4F;
+		Generics test = new Generics(6, 12, 3);
+		Integer maxNum = (Integer) test.maxNum();
+		Integer expec = 12;
 		assertEquals(expec, maxNum);
 			
 	}
 	
 	@Test
 	public void giveMaxNumAtThridPosition() {
-		Generics test = new Generics();
-		Float maxNum = test.maxNum(6.2F, 12.45F, 23.54F);
+		Generics test = new Generics(6, 12, 23);
+		Integer maxNum = (Integer) test.maxNum();
+		Integer expec = 23;
+		assertEquals(expec, maxNum);
+			
+	}
+	
+	@Test
+	public void giveMaxNumFloatAtFirstPosition() {
+		Generics test = new Generics(10.2F, 5.0F, 3.5F);
+		Float maxNum = (Float) test.maxNum();
+		Float expec = 10.2F;
+		assertEquals(expec, maxNum);
+			
+	}
+	
+	@Test
+	public void giveMaxNumFloatAtSecondPosition() {
+		Generics test = new Generics(6.5F, 12.4F, 3.6F);
+		Float maxNum = (Float) test.maxNum();
+		Float expec = 12.4F;
+		assertEquals(expec, maxNum);
+			
+	}
+	
+	@Test
+	public void giveMaxNumFloatAtThridPosition() {
+		Generics test = new Generics(6.2F, 12.45F, 23.54F);
+		Float maxNum = (Float) test.maxNum();
 		Float expec = 23.54F;
 		assertEquals(expec, maxNum);
 			
 	}
 
-}
+	@Test
+	public void givenFirstStringValueShouldReturnTrue() {
+		Generics test = new Generics("Peach", "Apple", "Banana");
+		String actual = (String) test.maxNum();
+		String expected = "Peach";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void givenSecondStringValueShouldReturnTrue() {
+		Generics test = new Generics("Apple", "Peach", "Banana");
+		String actual = (String) test.maxNum();
+		String expected = "Peach";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void givenThirdStringValueShouldReturnTrue() {
+		Generics test = new Generics("Banana", "Apple", "Peach");
+		String actual = (String) test.maxNum();
+		String expected = "Peach";
+		assertEquals(expected, actual);
+	}}
